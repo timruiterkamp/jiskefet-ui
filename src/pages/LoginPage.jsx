6397a8d23e16a571e254d6ce17e959c0b2cb36b2
Dennis Wegereef <denniswegereef@166.199.028.145.hva.nl>
2019-05-06 13:48:51 +0200

    working on logs page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+//import Col from "react-bootstrap/Col";
 
 export default class LoginPage extends Component {
   render() {
-    return <Button variant="warning">Primary</Button>;
+    return (
+      <Container>
+        <h1>Welcome to Jiskefet</h1>
+
+        <p>Please use your github account to sign in</p>
+        <Button variant="primary" href="/logs">
+          Log in
+        </Button>
+        <Button variant="secondary" href="https://github.com/">
+          Create account
+        </Button>
+      </Container>
+    );
   }
 }
