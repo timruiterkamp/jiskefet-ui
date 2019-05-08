d6d9bc2df834dd8c6092fcd37d729855dff657cc
Dennis Wegereef <denniswegereef@gmail.com>
2019-05-08 13:41:04 +0200

    For tim

--- src/pages/CreateLogs.jsx
+++ src/pages/CreateLogs.jsx
@@ -5,41 +5,54 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "../scss/layout/create-log.scss";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 export default class CreateLogs extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col sm={12}>
-            <h1>Create a new log</h1>
-          </Col>
-        </Row>
+      <React.Fragment>
+        <header>
+          <Container>
+            <Row>
+              <Col sm={12}>
+                <h3>Create a log</h3>
+              </Col>
+              <Col sm={8}>
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="inputGroup-sizing-default">
+                      Title
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
+                  />
+                </InputGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={8}>
+                <Editor wrapperClassName="demo-wrapper" />
 
-        <Row>
-          <Col sm={12}>
-            <Editor
-              wrapperClassName="demo-wrapper"
-              editorClassName="demo-editor"
-            />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col sm={3}>
-            <h2>Add logs</h2>
-          </Col>
-          <Col sm={3}>
-            <h2>Add runs</h2>
-          </Col>
-          <Col sm={3}>
-            <h2>Add tags</h2>
-          </Col>
-          <Col sm={3}>
-            <h2>Add files</h2>
-          </Col>
-        </Row>
-      </Container>
+                <textarea value="input" />
+              </Col>
+              <Col sm={4}>
+                <p>Add a attachments</p>
+                <input type="file" />
+                <Button variant="secondary mt-2">Submit</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={3}>
+                <h2>asdf</h2>
+              </Col>
+            </Row>
+          </Container>
+        </header>
+      </React.Fragment>
     );
   }
 }
